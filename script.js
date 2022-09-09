43c2fd77f5613d7370ca09c1ba1c1a4386db8c18
jackpipping <kaiwenx@gmail.com>
2022-09-08 23:43:22 -0400

    Added rounds to RockPaperScissorsGame

--- script.js
+++ script.js
@@ -18,13 +18,21 @@ function getComputerChoice(){
 
 }
 
-let computerChoice = getComputerChoice();
 
-console.log(computerChoice);
 
-const userChoice = prompt("Please enter your choice 1=rock 2=scissor 3= paper:");
 
-function playGame(userChoice, computerChoice){
+
+
+/*
+buttons.forEach(element => { element.addEventListener("click", checkChoice(element.id.value, computerChoice) )  
+});
+buttons.forEach(element => { element.addEventListener("click", displayConsole(element.id.value, computerChoice) )  
+});
+*/
+
+
+
+function checkChoice(userChoice, computerChoice){
     switch(true){
         case(userChoice == 1 && computerChoice == 3):
             return "lose";
@@ -44,6 +52,92 @@ function playGame(userChoice, computerChoice){
     }
 }
 
-console.log("computer:" + computerChoice);
-console.log("user:" + userChoice);
-console.log("1=rock 2=scissor 3= paper  " + playGame(userChoice, computerChoice));
+function displayRules(){
+    
+console.log("1=rock 2=scissor 3= paper ");
+}
+
+async function playRounds(){
+let scorePlayer = 0;
+let scoreComputer = 0;
+let scores = document.querySelector(".scores");
+let finalResult = document.querySelector(".finalResult");
+
+while(scoreComputer != 5 || scorePlayer != 5){
+
+
+let gameResult = await playGame();
+switch(gameResult){
+    case "win":
+        scorePlayer++;
+
+    case "lose":
+        scoreComputer++;
+    
+}
+scores.textContent = "Your score is " + scorePlayer + ", computer's score is "
++ scoreComputer;
+if(scoreComputer == 5){
+    finalResult.textContent = `Computer won ${scoreComputer} rounds!`;
+    console.log("Computer won five rounds!");
+    break;
+}else{
+    if(scorePlayer == 5){
+        finalResult.textContent = `You won ${scorePlayer} rounds!`;
+        console.log("You won five rounds!"); 
+        break; 
+    }
+    
+}
+}
+
+   
+}
+
+playRounds();
+
+function resolvePlayGame(){
+    return new Promise(resolve => {
+        resolve(playGame());
+      
+});
+
+}
+
+async function playGame(){
+let result = document.querySelector(".resultRound");
+let computerChoice = getComputerChoice();
+displayRules();
+console.log("Computer Choice is " + computerChoice);
+
+const playerChoice = await getPlayerChoice();
+
+result.textContent = "Computer played " + computerChoice + " You played "
++ playerChoice + " You "+ checkChoice(playerChoice, computerChoice);
+return checkChoice(playerChoice, computerChoice);
+         
+}
+
+
+
+
+function getPlayerChoice(){
+    return new Promise(resolve => {
+        let buttons = Array.from(document.querySelectorAll('.playerChoice'));
+
+        buttons.forEach(element => { element.addEventListener("click", 
+                function(event){
+                resolve(element.id);
+            })
+
+      });
+
+  
+         
+});
+}
+
+
+/*let button1 = document.getElementById("1");
+button1.addEventListener("click", function(event){console.log(button1.id)}); 
+*/
